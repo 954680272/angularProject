c57cf0192de1e9ff9cd7559007bc9a8729df6b9e
Zhb <dell>
2018-02-02 20:04:58 +0800

    node zhb

---
+++ ajax/db/db.js
@@ -0,0 +1,18 @@
+﻿const MongoClient  = require("mongodb").MongoClient;
+
+
+const DB_CONN_STR =  "mongodb://47.94.227.171:27017/admin"
+
+module.exports = {
+    conn:function(callback){
+        MongoClient.connect(DB_CONN_STR,(err,db)=>{
+            if(err){
+                console.log("数据库连接失败");
+                callback(err,null);
+            }else{
+                console.log("数据库连接成功")
+                callback(null,db);
+            }
+        })
+    }
+}
